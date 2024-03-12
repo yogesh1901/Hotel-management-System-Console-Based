b07dc82f00c6dd0a3ba98a9105a57ca90b2bb8e9
yogesh1901 <137715636+yogesh1901@users.noreply.github.com>
2024-03-12 11:54:38 +0530

    Add files via upload

---
+++ Holder.java
@@ -0,0 +1,10 @@
+package main;
+import java.io.Serializable;
+
+public class Holder implements Serializable 
+{
+DoubleRoom arr1[] = new DoubleRoom[10]; 
+DoubleRoom arr2[] = new DoubleRoom[20]; 
+SingleRoom arr3[] = new SingleRoom[10];
+SingleRoom arr4[] = new SingleRoom[20]; 
+}
